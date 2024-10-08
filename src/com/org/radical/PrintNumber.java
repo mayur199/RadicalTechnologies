9c316107375e84c36f949d6d09ec5d9feaa8c25d
mayur <kumbharemayur75@gmail.com>
2024-10-08 12:21:35 +0530

    Print the number

---
+++ src/com/org/radical/PrintNumber.java
@@ -0,0 +1,14 @@
+package com.org.radical;
+
+import java.util.Scanner;
+
+public class PrintNumber {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number : ");
+	
+		int num = sc.nextInt();
+		System.out.println("Entered number is : "+ num);
+		
+	}
+}
