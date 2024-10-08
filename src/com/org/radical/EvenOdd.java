bad81afaa77fb192539054a9813eb61e1ffaf515
mayur <kumbharemayur75@gmail.com>
2024-10-08 13:28:42 +0530

    Program to check if a number is Even or Odd

---
+++ src/com/org/radical/EvenOdd.java
@@ -0,0 +1,17 @@
+package com.org.radical;
+
+import java.util.Scanner;
+
+public class EvenOdd {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Number: ");
+		int num = sc.nextInt();
+		if (num % 2 == 0) {
+			System.out.println(num + " is an even number ");
+		} else {
+			System.out.println(num + " is an odd number ");
+		}
+
+	}
+}
