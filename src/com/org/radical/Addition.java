4fd5fabf3f02d723fd7c98f45651086b4e6845d2
mayur <kumbharemayur75@gmail.com>
2024-10-08 12:39:49 +0530

    Addition programs

---
+++ src/com/org/radical/Addition.java
@@ -0,0 +1,11 @@
+package com.org.radical;
+
+public class Addition {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		int add = a + b;
+		System.out.println(add);
+	}
+
+}
