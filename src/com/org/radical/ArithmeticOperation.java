1b33e580de894b02e9215b3a6d8301d8ba53bc60
mayur <kumbharemayur75@gmail.com>
2024-10-08 13:46:24 +0530

    Addition, Subtraction, multiplication and Division

---
+++ src/com/org/radical/ArithmeticOperation.java
@@ -0,0 +1,19 @@
+package com.org.radical;
+
+public class ArithmeticOperation {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		int Addition = a + b;
+		int Subtraction = a - b;
+		int Multiplication = a * b;
+		int Division = a / b;
+
+		System.out.println(Addition);
+		System.out.println(Subtraction);
+		System.out.println(Multiplication);
+		System.out.println(Division);
+
+	}
+
+}
