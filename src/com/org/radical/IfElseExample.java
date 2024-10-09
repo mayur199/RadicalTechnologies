1085e014bb988ce78eb701ee13ad5becaeabcc18
mayur <kumbharemayur75@gmail.com>
2024-10-09 19:53:50 +0530

    IfElse Example

---
+++ src/com/org/radical/IfElseExample.java
@@ -0,0 +1,19 @@
+package com.org.radical;
+
+public class IfElseExample {
+	public static void main(String[] args) {
+		int n1 = 20;
+		int n2 = 30;
+
+		if (n1 == n2) {
+			System.out.println(n1 + " is equal to " + n2);
+		} else {
+			if (n1 > n2) {
+				System.out.println(n1 + " is greater than " + n2);
+			} else {
+				System.out.println(n1 + " is smaller than " + n2);
+
+			}
+		}
+	}
+}
