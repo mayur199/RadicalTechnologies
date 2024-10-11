fed0f607afb7bdb66e56b15977857882426d1d9b
mayur <kumbharemayur75@gmail.com>
2024-10-11 19:45:06 +0530

    Java program to print Hello World in Hindi and Japanese

---
+++ src/com/org/radical/HelloWorldUnicode.java
@@ -0,0 +1,12 @@
+package com.org.radical;
+
+public class HelloWorldUnicode {
+    public static void main(String[] args) {
+    	
+        System.out.println("\u0048\u0065\u006C\u006C\u006F\u002C \u0057\u006F\u0072\u006C\u0064\u0021");
+
+        System.out.println("\u0928\u092E\u0938\u094D\u0924\u0947 \u0926\u0941\u0928\u093F\u092F\u093E!"); // नमस्ते दुनिया! (Hindi for "Hello, World!")
+        System.out.println("\u3053\u3093\u306B\u3061\u306F\u4E16\u754C!"); // こんにちは世界! (Japanese for "Hello, World!")
+
+    }
+}
