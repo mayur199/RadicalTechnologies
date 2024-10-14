73dd6633921112f55c57623b9ed21640a1e76838
mayur <kumbharemayur75@gmail.com>
2024-10-14 20:07:28 +0530

    maximum of two number

---
+++ src/com/org/radical/MaxOfTwo.java
@@ -0,0 +1,20 @@
+package com.org.radical;
+
+import java.util.Scanner;
+
+public class MaxOfTwo {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the first number :");
+		int a = sc.nextInt();
+		System.out.println("Enter the second number :");
+		int b = sc.nextInt();
+		if (a > b) {
+			System.out.println("a is greater than b");
+		} else if (b > a) {
+			System.out.println("b is greater than a");
+		} else {
+			System.out.println("Bothe are equal");
+		}
+	}
+}
