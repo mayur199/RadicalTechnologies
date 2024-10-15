0aa44628c27e3d7422586aab7adc65db9af717c3
mayur <kumbharemayur75@gmail.com>
2024-10-15 19:43:48 +0530

    Varibles Examples

---
+++ src/com/org/radical/VariablesExample.java
@@ -0,0 +1,18 @@
+package com.org.radical;
+
+public class VariablesExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int num = 10;
+		String name = "John";
+		String greeting = "Hello";
+
+		System.out.println("Numbers: " + num);
+		System.out.println("Name: " + name);
+		System.out.println("Greeting: " + greeting);
+
+	}
+
+}
---
+++ src/com/org/radical/VariablesExample1.java
@@ -0,0 +1,24 @@
+package com.org.radical;
+
+public class VariablesExample1 {
+	public static void main(String[] args) {
+
+		int a = 10;
+		byte x = 20;
+		short y = 2000;
+		long z = 38838738L;
+		float m = 8.98f;
+		double n = 22.221;
+		char w = 'A';
+		boolean guess = true;
+		System.out.println(a);
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+		System.out.println(m);
+		System.out.println(n);
+		System.out.println(w);
+		System.out.println(guess);
+
+	}
+}
