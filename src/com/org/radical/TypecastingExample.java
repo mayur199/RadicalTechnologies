1e7928154874cad3a0908c92ff3ed15578ca7d38
mayur <kumbharemayur75@gmail.com>
2024-10-16 19:12:23 +0530

    Typecast a value from one type to other type

---
+++ src/com/org/radical/TypecastingExample.java
@@ -0,0 +1,11 @@
+package com.org.radical;
+
+public class TypecastingExample {
+	public static void main(String[] args) {
+		int a= 7;
+		float b;
+		b= (float)a;
+		System.out.println(a +" "+b);
+		
+	}
+}
