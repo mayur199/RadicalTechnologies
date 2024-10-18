778101172910538c92a960f75cfca092567f1f57
mayur <kumbharemayur75@gmail.com>
2024-10-18 22:13:07 +0530

    This program is to check whether the entered number is postive, negative or zero

---
+++ src/com/org/radical/PositiveNegativeCheck.java
@@ -0,0 +1,20 @@
+package com.org.radical;
+
+import java.util.Scanner;
+
+public class PositiveNegativeCheck {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number :");
+		int num = sc.nextInt();
+		if (num > 0) {
+			System.out.println("Positive Number");
+		} else if (num < 0) {
+			System.out.println("Negative Number");
+		} else {
+			System.out.println("Number is Zero");
+		}
+
+	}
+
+}
