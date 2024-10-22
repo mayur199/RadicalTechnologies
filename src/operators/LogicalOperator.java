ecfb7680f50c3247c64a20d39e3f1cb494bc1e75
mayur <kumbharemayur75@gmail.com>
2024-10-22 19:33:10 +0530

    Logical Operator Example

---
+++ src/operators/LogicalOperator.java
@@ -0,0 +1,16 @@
+package operators;
+
+public class LogicalOperator {
+	public static void main(String[] args) {
+
+		int a = 5;
+		int b = 10;
+		int c = 15;
+		boolean x = true;
+
+		System.out.println((a < b) && (b < c)); // true (Logical AND)
+		System.out.println((a > b) || (b > c)); // false (Logical OR)
+		System.out.println(!x); // false (Logical NOT)
+	}
+
+}
