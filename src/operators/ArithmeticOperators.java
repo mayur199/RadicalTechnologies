ee8cd8f917c7b37644937f12e1e41a74ec95f839
mayur <kumbharemayur75@gmail.com>
2024-10-22 19:05:39 +0530

    Arithmetic Operators Example

---
+++ src/operators/ArithmeticOperators.java
@@ -0,0 +1,24 @@
+package operators;
+
+public class ArithmeticOperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a = 10;
+		int b = 3;
+
+		int add = a + b;
+		int sub = a - b;
+		int multi = a * b;
+		int div = a / b;
+		int mod = a % b;
+
+		System.out.println("Addition : " + add);
+		System.out.println("Subtraction : " + sub);
+		System.out.println("Multiplication :" + multi);
+		System.out.println("Division : " + div);
+		System.out.println("Modulus :" + mod);
+	}
+
+}
