a0a799cb168d7165956185a93b64370659e53004
mayur <kumbharemayur75@gmail.com>
2024-10-25 11:33:21 +0530

    Ternary Operator Example

---
+++ src/operators/TernaryOperator.java
@@ -0,0 +1,36 @@
+package operators;
+
+public class TernaryOperator {
+	public static void main(String[] args) {
+
+		/**
+		 * max of two number
+		 */
+//		int a = 10;
+//		int b = 20;
+//
+//		int max = (a > b) ? a : b;
+//		System.out.println(max);
+
+//		/**
+//		 * max of three number
+//		 */
+//		int a = 3;
+//		int b = 6;
+//		int c = 4;
+//
+//		int max = (a > b) && (a > c) ? a : (b > c) ? b : c;
+//		System.out.println(max);
+
+		/**
+		 * Even Odd
+		 */
+
+		int a = 5;
+
+		String result = (a % 2 == 0) ? "Even" : "Odd";
+		System.out.println(result);
+
+	}
+
+}
