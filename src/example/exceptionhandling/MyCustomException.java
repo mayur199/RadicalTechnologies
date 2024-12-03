450bbf7a792728b44ad915e4e1f852cf2a5450c9
mayur <kumbharemayur75@gmail.com>
2024-12-03 14:29:16 +0530

    Exception Handling Example

---
+++ src/example/exceptionhandling/MyCustomException.java
@@ -0,0 +1,11 @@
+package example.exceptionhandling;
+
+public class MyCustomException extends Exception {
+
+	private static final long serialVersionUID = 7415449567376675406L;
+	
+	public MyCustomException(String msg) {
+		super(msg);
+	}
+	
+}
