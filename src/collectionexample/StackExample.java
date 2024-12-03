d0730e3c9d9f78a0135097620e8e9f100570cf4c
mayur <kumbharemayur75@gmail.com>
2024-12-03 23:31:07 +0530

    Implemented a custom Stack for managing books with push, pop, peek, and print functionality, including exception handling.

---
+++ src/collectionexample/StackExample.java
@@ -0,0 +1,57 @@
+package collectionexample;
+
+import java.util.ArrayList;
+import java.util.List;
+import collections.Book;
+import example.exceptionhandling.MyCustomException;
+
+public class StackExample {
+
+	private List<Book> bookStack = new ArrayList<>();
+
+	public void push(Book book) {
+		bookStack.add(book);
+	}
+
+	public Book pop() throws MyCustomException {
+		if (bookStack.isEmpty()) {
+			throw new MyCustomException("Stack is empty"); // Exception if stack is empty
+		}
+		return bookStack.remove(bookStack.size() - 1); // Remove and return the last added book (LIFO)
+	}
+
+	public Book peek() throws MyCustomException {
+		if (bookStack.isEmpty()) {
+			throw new MyCustomException("Stack is empty"); // Exception if stack is empty
+		}
+		return bookStack.get(bookStack.size() - 1); // Return the last book without removing it
+	}
+
+	// Display all books
+	public void printStack() throws MyCustomException {
+		if (bookStack.isEmpty()) {
+			throw new MyCustomException("Stack is empty"); // Exception if stack is empty
+		}
+		for (Book book : bookStack) {
+			System.out.println(book); // Print each book in the stack
+		}
+	}
+
+	public static void main(String[] args) {
+		StackExample myStack = new StackExample();
+
+		myStack.push(new Book("Java Fundamentals", "Author A", "Programming"));
+		myStack.push(new Book("Data Structures", "Author B", "Computer Science"));
+		myStack.push(new Book("Algorithms", "Author C", "Computer Science"));
+
+		try {
+			System.out.println("Peek: " + myStack.peek());
+			System.out.println("Popped: " + myStack.pop());
+			System.out.println("Popped: " + myStack.pop());
+			System.out.println("Books in Stack:");
+			myStack.printStack();
+		} catch (MyCustomException e) {
+			e.printStackTrace();
+		}
+	}
+}
