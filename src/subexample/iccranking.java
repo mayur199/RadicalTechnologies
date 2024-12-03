9d652edd6e34b56034333c1e74bdb4f8375cbcca
mayur <kumbharemayur75@gmail.com>
2024-12-03 14:03:33 +0530

    new changes

---
+++ src/subexample/iccranking.java
@@ -0,0 +1,16 @@
+package subexample;
+
+public class iccranking {
+
+	// Protected method (accessible within the same package or via inheritance in
+	// another package)
+	protected String top3testbatsmen() {
+		return "1. Marnus Labuschagne \n2. Joe Root\n3. Kane Williamson";
+	}
+
+	// Default method (accessible only within the same package)
+	String top3ODIbatsmen() {
+		return "1. Babar Azam\n2. Rohit Sharma\n3. David Warner";
+	}
+}
+			 
