bf427d409dd53841b59e2a95ded98b71ce0f4afb
mayur <kumbharemayur75@gmail.com>
2024-12-03 23:47:55 +0530

    Package name changed

---
+++ src/immutabledemoeg/ImmutablePerson.java
@@ -0,0 +1,23 @@
+package immutabledemoeg;
+
+//Immutable Class
+final class ImmutablePerson {
+	private final String name;
+	private final int age;
+
+	// Constructor
+	public ImmutablePerson(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	// Getter for name
+	public String getName() {
+		return name;
+	}
+
+	// Getter for age
+	public int getAge() {
+		return age;
+	}
+}
