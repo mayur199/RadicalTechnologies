bcdf5d8a1995f18b84537c280369324b9443a770
mayur <kumbharemayur75@gmail.com>
2024-12-03 14:41:09 +0530

    Demonstrated different number systems (decimal, binary, octal, hexadecimal) in Java.

---
+++ src/example/NumberSystems.java
@@ -0,0 +1,21 @@
+package example;
+
+public class NumberSystems {
+	public static void main(String[] args) {
+		// Decimal number
+		int decimalNumber = 100;
+		System.out.println("Decimal: " + decimalNumber);
+
+		// Binary number
+		int binaryNumber = 0b1100100; // Binary representation of 100
+		System.out.println("Binary: " + binaryNumber);
+
+		// Octal number
+		int octalNumber = 0144; // Octal representation of 100
+		System.out.println("Octal: " + octalNumber);
+
+		// Hexadecimal number
+		int hexadecimalNumber = 0x64; // Hexadecimal representation of 100
+		System.out.println("Hexadecimal: " + hexadecimalNumber);
+	}
+}
