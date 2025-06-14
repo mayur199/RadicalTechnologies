7ad030321089ef36f25d8a87864e4005d01ff77d
mayur <kumbharemayur75@gmail.com>
2025-06-14 11:48:19 +0530

    update1

--- src/collectionexample/QueueUsingList.java
+++ src/collectionexample/QueueUsingList.java
@@ -42,7 +42,7 @@ public class QueueUsingList {
 		}
 	}
 
-	//Implement
+	//Implement  main method to test the queue functionality
 	public static void main(String[] args) {
 		QueueUsingList queue = new QueueUsingList();
 
